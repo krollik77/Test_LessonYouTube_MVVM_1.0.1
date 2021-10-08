66437c00b8c22e72b768b9df9eaa5e40387e007b
krollik77 <krollik77@ya.ru>
2021-10-08 10:53:11 +0300

    Добавьте файлы проекта.

---
+++ Model/DataWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Wpf_MVVM_1._0._1.Model
+{
+    public static class DataWorker
+    {
+
+        // Создать отдел //
+
+        // Создать позицию //
+
+        // Создать сотрудника //
+
+        // Удалить отдел //
+
+        // Удалить позицию //
+
+        // Удалить сотрудника //
+
+        // Редактирование отдел //
+
+        // Редактирование позицию //
+
+        // Редактирование сотрудника //
+
+    }
+
+}
